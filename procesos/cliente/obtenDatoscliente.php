562535300e5aaecf0dfc0fb7c7eb5d1336ec43ae
david <doncastillo404@gmail.com>
2024-05-30 22:22:57 -0600

    se prepara para la fucion de la rama

--- procesos/cliente/obtenDatoscliente.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	
-	require_once "../../clases/Conexion.php";
-	require_once "../../clases/Clientes.php";
-
-	$obj= new Clientes();
-
-	echo json_encode($obj->obtenDatosCliente($_POST['idcliente']));
- ?>
